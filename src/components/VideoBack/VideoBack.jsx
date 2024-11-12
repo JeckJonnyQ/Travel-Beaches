e0b83d9c32d9438f0f23f0c494b23af6c2768c9e
Reut Vladislav <reut-2011@mail.ru>
2024-11-12 12:58:58 +0300

    refactor: change settings for video back on safari

--- src/components/VideoBack/VideoBack.jsx
+++ src/components/VideoBack/VideoBack.jsx
@@ -3,7 +3,14 @@ import VideoBackground from "../../assets/VideoBackground.mp4";
 
 export default function VideoBack() {
   return (
-    <video className="video-background" autoPlay muted loop preload="auto">
+    <video
+      className="video-background"
+      autoPlay
+      muted
+      loop
+      preload="auto"
+      playsInline
+    >
       <source type="video/mp4" src={VideoBackground} />
     </video>
   );
