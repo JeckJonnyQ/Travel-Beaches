d802bf61c2c1942f6cd23de71534442248c7e350
Reut Vladislav <reut-2011@mail.ru>
2024-11-12 15:06:08 +0300

    feat: change video component

--- src/components/VideoBack/VideoBack.jsx
+++ src/components/VideoBack/VideoBack.jsx
@@ -3,15 +3,17 @@ import VideoBackground from "../../assets/VideoBackground.mp4";
 
 export default function VideoBack() {
   return (
-    <video
-      className="video-background"
-      autoPlay
-      muted
-      loop
-      preload="auto"
-      playsInline
-    >
-      <source type="video/mp4" src={VideoBackground} />
-    </video>
+    <div className="video-container">
+      <video
+        className="video-background"
+        autoPlay
+        muted
+        loop
+        preload="auto"
+        playsInline
+      >
+        <source type="video/mp4" src={VideoBackground} />
+      </video>
+    </div>
   );
 }
