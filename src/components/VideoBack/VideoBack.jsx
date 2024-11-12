82035a3e1a94e54863bc03146f9a74d8995bda66
Reut Vladislav <reut-2011@mail.ru>
2024-11-12 15:44:05 +0300

    feat: testing video component

--- src/components/VideoBack/VideoBack.jsx
+++ src/components/VideoBack/VideoBack.jsx
@@ -1,10 +1,49 @@
+// import "./VideoBack.scss";
+// import VideoBackground from "../../assets/VideoBackground.mp4";
+
+// export default function VideoBack() {
+//   return (
+//     <div className="video-container">
+//       <video
+//         className="video-background"
+//         autoPlay
+//         muted
+//         loop
+//         preload="auto"
+//         playsInline
+//       >
+//         <source type="video/mp4" src={VideoBackground} />
+//       </video>
+//     </div>
+//   );
+// }
+
+import { useState, useRef, useEffect } from "react";
 import "./VideoBack.scss";
 import VideoBackground from "../../assets/VideoBackground.mp4";
 
 export default function VideoBack() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const handleCanPlayThrough = () => {
+      setIsLoaded(true);
+    };
+
+    video.addEventListener("canplaythrough", handleCanPlayThrough);
+    return () => {
+      video.removeEventListener("canplaythrough", handleCanPlayThrough);
+    };
+  }, []);
+
   return (
     <div className="video-container">
+      {!isLoaded && <div className="loading-indicator">Загрузка...</div>}
+
       <video
+        ref={videoRef}
         className="video-background"
         autoPlay
         muted
